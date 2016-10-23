549d23c6fba675fd877b31829760b85c7da52a6e
Tomas Stuina <Tomas Stuina>
2016-10-23 20:04:33 +0200

    Adding program.hs.

---
+++ program.hs
@@ -0,0 +1,21 @@
+module Program1
+where
+
+trimString :: String -> String
+trimString ('\"':t) = reverse $ trimString $ reverse t
+trimString h = h
+
+readDigit :: String -> Int
+readDigit ('0':t) = 0
+readDigit ('1':t) = 1
+readDigit ('2':t) = 2
+readDigit _ = error "Negalima koordinates reiksme"
+
+readValue :: String -> String  -> (String, String)
+readValue [] [] = ([], [])
+readValue [] b = (b, [])
+readValue (h:t) b 
+  | h == ']' = (b, t)
+  | h == ';' = (b, t)
+  | h == ' ' || h == '\"' = readValue t b
+  | otherwise = readValue t (b ++ [h])
